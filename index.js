dbf23d7a7c256e00194bbfda3c46be1205a82021
Tsholos-lab <masegompshe@icloud.com>
2024-09-11 13:41:09 +0200

    Injected forecast html to JS file

--- index.js
+++ index.js
@@ -54,7 +54,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+        <strong>15°</strong>
+        </div>
+        <div class="weather-forecast-temperature">
+        <strong> 9°</strong>
+        </div>
+        </div>
+        </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
